4328b0b4d70deec13726b449677744c3366d7080
Alex Hoffmann <alhoffmann@gmail.com>
2023-10-21 01:51:03 +0400

    some fixes

--- tests/input/download_vision_model.sh
+++ tests/input/download_vision_model.sh
@@ -1,9 +1,5 @@
 echo "Downloading pretrained vision model..."
 url=http://i.stanford.edu/hazy/share/fonduer/visualtable/paleo_visual_model.h5
 data_file=paleo_visual_model.h5
-if type curl &>/dev/null; then
-    curl -RLO $url
-elif type wget &>/dev/null; then
-    wget -N -nc $url
-fi 
+curl -RLO $url
 echo "Done!"
